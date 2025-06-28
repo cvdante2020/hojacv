14c11c005de9feb68868427cad15fab93be1d4e9
cvdante2020 <dante_emc@hotmail.com>
2025-06-27 22:32:13 -0500

    Ignorar ESLint en producción temporalmente2

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero() {
   className="overflow-hidden rounded-xl border-4 border-cyan-500 shadow-lg bg-white"
 >
   <Image
-    src="/profile.jpg"
+    src="/profile1.jpg"
     alt="Perfil"
     width={220}
     height={280}
